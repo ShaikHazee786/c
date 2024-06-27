9a28d7aeced8ae010f8452702941468e2253222b
ShaikHazee786 <hazeeshaik679@gmail.com>
2024-06-27 09:47:29 +0530

    Add files via upload

---
+++ probem-4a.c
@@ -0,0 +1,21 @@
+    #include<stdio.h>
+    int main()
+    {
+    	int w;
+    	scanf("%d",&w);
+    	if(1>=w<=100)
+    	{
+    		if(w%2==0&&w>=4)
+    		{
+    	            printf("YES");
+    	 }       
+    		else
+    		{
+    		printf("NO");
+    	           }
+    	}
+    	else
+    	{
+    	prinf("NO");	
+    	}
+    }
